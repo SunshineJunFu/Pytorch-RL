482111113c0d8a798827b5bf6edabfd0bdcbd1b1
Jun Fu <33153297+SunshineJunFu@users.noreply.github.com>
2019-02-15 22:57:37 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-02-11 21:37:11
+# @Author  : Jun Fu 
+# @Email   : fujun@mail.ustc.edu.cn
+
+import tensorflow as tf 
+
+class model(object):
+
+	def __init__():
+		pass
+
+	def build_model():
+		
+		inputs = tf.placeholder(tf.float32, shape=[None, 1]) 
+
+		dense1 = tf.layers.dense(inputs, 128)
+
+	def train():
+		pass
+
+	def eval():
+		pass
+
+	def summary():
+		pass
+
+	def restore():
+		pass
+
+	def save():
+		pass
